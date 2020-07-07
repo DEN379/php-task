b957b1fddd7ded525c249fae21c46a15d8523a28
Den <denyssakadel@gmail.com>
2020-07-07 16:49:35 +0300

    routes fixes

--- app/views/tasks/edit.php
+++ app/views/tasks/edit.php
@@ -3,7 +3,7 @@
 <div class="container">
     <H1 class="home"><a href="/app/tasks">My Tasks</a></H1>
         <h1 class="">Edit task</h1>
-        <form action="update.php?id=<?= $task['id'];?>" method="post">
+        <form action="./update" method="post">
             <div class="form-group create-form">
                 <label for="create-title">Enter title for task:</label>
                 <input type="text" name="title" id="create-title" placeholder="Title" class="form-control" value="<?= $task['title']; ?>">
--- app/views/tasks/show.php
+++ app/views/tasks/show.php
@@ -2,8 +2,8 @@
 
 <div class="container">
     <H1 class="home"><a href="/app/tasks">My Tasks</a></H1>
-        <label for="title">Title: </label>
-        <h1 id="title"><?=$task['title']; ?></h1>
-        <label for="description">Description: </label>
-        <h2 id="description"><?= $task['descr'];?></h2>
-    </div>
+    <label for="title">Title: </label>
+    <h1 id="title"><?=$task['title']; ?></h1>
+    <label for="description">Description: </label>
+    <h2 id="description"><?= $task['descr'];?></h2>
+</div>
